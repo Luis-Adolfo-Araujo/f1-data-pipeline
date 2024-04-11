dc06a19a14b7f390f53c4a8976e7218e98a486b2
Luis-Adolfo-Araujo <laaan@cesar.school>
2024-04-10 23:37:45 -0300

    code enhancement

--- jobs/s1.py
+++ jobs/s1.py
@@ -2,16 +2,16 @@
 import pandas as pd
 import requests
 import csv
- 
+
+BASE_URL = "https://api.openf1.org/v1/"
 methods = ['car_data', 'drivers', 'intervals', 'laps', 'location', 'meetings', 'pit', 'position', 'race_control', 'sessions', 'stints', 'team_radio', 'weather']
 
 for i in range(len(methods)):
-    req = requests.get(f"https://api.openf1.org/v1/{methods[i]}")
+    uri = methods[i]
+    req = requests.get(BASE_URL+uri)
+    print(req.url)
     print(req.status_code)
     
     with open(f'../staging/{methods[i]}.csv', 'w') as csvfile:
         csvwiter = csv.writer(csvfile)
         csvwiter.writerow(req)
-
-
-# print(re.content)
